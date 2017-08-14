51605e39f9cc7c01c7da6d2288af2c2c53487743
yuxuan39 <kumo9102151@gmail.com>
2017-08-14 14:59:32 +0800

    add alert

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 import './styles/style.scss'
-import shortid from 'shortid'
+import shortid from 'short-id'
 
 var inputs = document.querySelectorAll('.login-input')
 var username = document.getElementById('username')
 var email = document.getElementById('email')
 var phone = document.getElementById('phone')
-var birth = document.getElementById('birth')
+var birth = document.getElementById('birth') 
 
 
-    inputs.addEventListener('keydown', function (event) {
-        var type = event.target.type
-        
-    })
+
+username.addEventListener('keydown', function(event) {
+    var originalHTML = username.innerHTML
+    var length = event.target.value.length + 1
+    var dd = document.getElementById('username-dd')
+    var text = document.createTextNode('It\'s too short (minimum is 6 characters)')
+    if(length < 6) {
+        dd.appendChild(text)
+        username.innerHTML = originalHTML
+    }
+    else if(length >= 6){
+        dd.removeChild(text)
+    }
+})
+
+/* 
+
+        var type = typeof event.target.value
+        console.log(type)
+        if (type === 'number') {
+            console.log('YES')
+        }
+
+
 
 function isText (){
-    if (type === 'text') {
-            isSixWords(event)
-        }
+    
 function isSixWords(event) {
 }
     
@@ -24,4 +42,4 @@ function isSixWords(event) {
             appendChild('<p>It\'s too short (minimum is 6 characters)</p>')
         })
 function input2() { }
-function inputDate() { }
+function inputDate() { }*/
